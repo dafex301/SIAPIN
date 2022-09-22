5d86dc2a280833becc3e9c6e609c20020dcb02ac
Rif'an Fatoni Febrianto <73413289+Rifaaanf@users.noreply.github.com>
2022-09-22 15:37:57 +0700

    fix left menu css

--- resources/views/dashboard/components/menu_left.blade.php
+++ resources/views/dashboard/components/menu_left.blade.php
@@ -1,112 +1,28 @@
 <div class="bg-white rounded-lg shadow-lg mt-4 flex flex-col">
     <div>
         <div class="flex items-center flex-row py-3 pl-3 hover:bg-gray-100 rounded-t-lg cursor-pointer ">
-            <i class=@php
-                // change color based on route
-                if (request()->routeIs('dashboard')) {
-                    echo '"fa-solid fa-house pr-2"';
-                } else {
-                    echo '"fa-solid fa-house pr-2 text-gray-400"';
-                }
-            @endphp></i>
-            <p class=@php
-                // change color based on route
-                if (request()->routeIs('dashboard')) {
-                    echo '';
-                } else {
-                    echo 'text-gray-400';
-                }
-            @endphp>Home</p>
+            <i class="{{ (request() -> is('dashboard')) ? 'fa-solid fa-house pr-2' : 'fa-solid fa-house pr-2 text-gray-400'}}"></i>
+            <p class="{{ (request() -> is('dashboard')) ? '' : 'text-gray-400'}}">Home</p>
         </div>
         <div class="flex items-center flex-row py-3 pl-3 hover:bg-gray-100 cursor-pointer">
-            <i class=@php
-                // change color based on route
-                if (request()->routeIs('dashboard/mahasiswa')) {
-                    echo '"fa-solid fa-user-group pr-2"';
-                } else {
-                    echo '"fa-solid fa-user-group pr-2 text-gray-400"';
-                }
-            @endphp></i>
-            <p class=@php
-                // change color based on route
-                if (request()->routeIs('dashboard/mahasiswa')) {
-                    echo '';
-                } else {
-                    echo 'text-gray-400';
-                }
-            @endphp>Mahasiswa</p>
+            <i class="{{ (request() -> is('mahasiswa')) ? 'fa-solid fa-user-group pr-2' : 'fa-solid fa-user-group pr-2 text-gray-400'}}"></i>
+            <p class="{{ (request() -> is('mahasiswa')) ? '' : 'text-gray-400'}}">Mahasiswa</p>
         </div>
         <div class="flex items-center flex-row py-3 pl-3 hover:bg-gray-100 cursor-pointer">
-            <i class=@php
-                // change color based on route
-                if (request()->routeIs('dashboard/lab')) {
-                    echo '"fa-solid fa-computer pr-2"';
-                } else {
-                    echo '"fa-solid fa-computer pr-2 text-gray-400"';
-                }
-            @endphp></i>
-            <p class=@php
-                // change color based on route
-                if (request()->routeIs('dashboard/lab')) {
-                    echo '';
-                } else {
-                    echo 'text-gray-400';
-                }
-            @endphp>Lab</p>
+            <i class="{{ (request() -> is('dashboard/lab')) ? 'fa-solid fa-computer pr-2' : 'fa-solid fa-computer pr-2 text-gray-400'}}"></i>
+            <p class="{{ (request() -> is('dashboard/lab')) ? '' : 'text-gray-400'}}">Lab</p>
         </div>
         <div class="flex items-center flex-row py-3 pl-3 hover:bg-gray-100 cursor-pointer">
-            <i class=@php
-                // change color based on route
-                if (request()->routeIs('dashboard/matkul')) {
-                    echo '"fa-solid fa-book pr-2"';
-                } else {
-                    echo '"fa-solid fa-book pr-2 text-gray-400"';
-                }
-            @endphp></i>
-            <p class=@php
-                // change color based on route
-                if (request()->routeIs('dashboard/matkul')) {
-                    echo '';
-                } else {
-                    echo 'text-gray-400';
-                }
-            @endphp>Mata Kuliah</p>
+            <i class="{{ (request() -> is('dashboard/matkul')) ? 'fa-solid fa-book pr-2' : 'fa-solid fa-book pr-2 text-gray-400'}}"></i>
+            <p class="{{ (request() -> is('dashboard/matkul')) ? '' : 'text-gray-400'}}">Mata Kuliah</p>
         </div>
         <div class="flex items-center flex-row py-3 pl-3 hover:bg-gray-100 cursor-pointer">
-            <i class=@php
-                // change color based on route
-                if (request()->routeIs('dashboard/jadwal')) {
-                    echo '"fa-solid fa-calendar pr-2"';
-                } else {
-                    echo '"fa-solid fa-calendar pr-2 text-gray-400"';
-                }
-            @endphp></i>
-            <p class=@php
-                // change color based on route
-                if (request()->routeIs('dashboard/jadwal')) {
-                    echo '';
-                } else {
-                    echo 'text-gray-400';
-                }
-            @endphp>Jadwal</p>
+            <i class="{{ (request() -> is('dashboard/jadwal')) ? 'fa-solid fa-calendar pr-2' : 'fa-solid fa-calendar pr-2 text-gray-400'}}"></i>
+            <p class="{{ (request() -> is('dashboard/jadwal')) ? '' : 'text-gray-400'}}">Jadwal</p>
         </div>
         <div class="flex items-center flex-row py-3 pl-3 hover:bg-gray-100 cursor-pointer">
-            <i class=@php
-                // change color based on route
-                if (request()->routeIs('dashboard/presensi')) {
-                    echo '"fa-solid fa-bell pr-2"';
-                } else {
-                    echo '"fa-solid fa-bell pr-2 text-gray-400"';
-                }
-            @endphp></i>
-            <p class=@php
-                // change color based on route
-                if (request()->routeIs('dashboard/presensi')) {
-                    echo '';
-                } else {
-                    echo 'text-gray-400';
-                }
-            @endphp>Presensi</p>
+            <i class="{{ (request() -> is('dashboard/presensi')) ? 'fa-solid fa-bell pr-2' : 'fa-solid fa-bell pr-2 text-gray-400'}}"></i>
+            <p class="{{ (request() -> is('dashboard/presensi')) ? '' : 'text-gray-400'}}">Presensi</p>
         </div>
     </div>
 </div>
