0dd13136d3c1b211e706ce0c12daff83b4f47e95
Fahrel Gibran Alghany <fahrel@students.undip.ac.id>
2022-10-01 20:03:15 +0700

    small message on dashboard

--- resources/views/dashboard/components/main_right.blade.php
+++ resources/views/dashboard/components/main_right.blade.php
@@ -4,6 +4,13 @@
         <h2 class="text-left font-bold text-2xl pl-5 pt-4">
             Dashboard
         </h2>
+        {{-- Hello user->name --}}
+        <div class="flex flex-col items-center mt-5">
+            <h2 class="text-3xl font-semibold mt-5">
+                Selamat Datang, {{ Auth::user()->nama }}
+            </h2>
+            <h2 class="text-xl">Semoga harimu "menyenangkan" 😁</h2>
+        </div>
     @endif
     @yield('main_content')
 </div>
